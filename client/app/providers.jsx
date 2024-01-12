a1706249f20521c73eb56e390b507ba3ad7842ca
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-12 16:04:23 -0500

    create providers component and import/export nextui provider

---
+++ client/app/providers.jsx
@@ -0,0 +1,13 @@
+"use client";
+import { NextUIProvider } from "@nextui-org/system";
+
+const Providers = ({ children }) => {
+  return (
+    <NextUIProvider>
+      {/*  */}
+      {children}
+    </NextUIProvider>
+  );
+};
+
+export default Providers;
