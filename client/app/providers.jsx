678bde0b62eafe0eaa060fb52644bd68f9b6f63a
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-12 16:09:36 -0500

    import/wrap providers component at root

--- client/app/providers.jsx
+++ client/app/providers.jsx
@@ -1,13 +1,13 @@
 "use client";
-import { NextUIProvider } from "@nextui-org/system";
+import { NextUIProvider } from "@nextui-org/react";
 
-const Providers = ({ children }) => {
+export function Providers({ children }) {
   return (
-    <NextUIProvider>
-      {/*  */}
-      {children}
-    </NextUIProvider>
+    <>
+      <NextUIProvider>
+        {/*  */}
+        {children}
+      </NextUIProvider>
+    </>
   );
-};
-
-export default Providers;
+}
