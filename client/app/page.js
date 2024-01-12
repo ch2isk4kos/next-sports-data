78d7956517e99e2369de2dd7e644d9bc363029e5
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-12 15:42:33 -0500

    rename server component

--- client/app/page.js
+++ client/app/page.js
@@ -1,7 +1,9 @@
-export default function Home() {
+import Home from "../components/home";
+
+export default function Homepage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home Page</h1>
+      <Home />
     </main>
   );
 }
