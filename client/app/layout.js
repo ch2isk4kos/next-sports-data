91fea19510e61f56a3bab99a32432879bfe2ad1c
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-12 19:16:18 -0500

    apply global style for screen height

--- client/app/layout.js
+++ client/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>
+      <body className={`${inter.className} min-h-screen`}>
         <Providers>
           <GlobalNavbar />
           {children}
