90b4b53cc6238b409551cdfb65cd9d2dc6c87947
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-12 19:17:30 -0500

    apply tailwind attributes for nfl homepage

--- client/app/nfl/page.js
+++ client/app/nfl/page.js
@@ -3,7 +3,7 @@
 export default function NFLHomepage() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <main className="flex justify-center items-center ">
+    <main className="flex justify-center items-center">
       <h1>NFL Homepage</h1>
     </main>
   );
