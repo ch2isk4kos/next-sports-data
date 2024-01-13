2084f7774d31002df832ffcdd26c12a67e76310c
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-12 19:16:59 -0500

    create route/page for nfl route

---
+++ client/app/nfl/page.js
@@ -0,0 +1,10 @@
+// import Home from "../components/home";
+
+export default function NFLHomepage() {
+  return (
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex justify-center items-center ">
+      <h1>NFL Homepage</h1>
+    </main>
+  );
+}
