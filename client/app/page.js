5c6e21eb2c19d2c74b6a5cbbd5952473d5f1c21e
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-12 19:16:38 -0500

    remove tailwind attributes

--- client/app/page.js
+++ client/app/page.js
@@ -2,7 +2,7 @@ import Home from "../components/home";
 
 export default function Homepage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex justify-center items-center">
       <Home />
     </main>
   );
