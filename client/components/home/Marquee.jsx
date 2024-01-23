0a0767025969eb4bc9def44dcb3a70b24ed1e8c9
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-23 09:29:40 -0500

    import motion library and pass marquee varients to rendering element(s)

--- client/components/home/Marquee.jsx
+++ client/components/home/Marquee.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const marquee = {
+  animate: {
+    x: [0, -1035],
+    transition: {
+      x: {
+        repeat: Infinity,
+        repeateType: "loop",
+        duration: 5,
+        ease: "linear",
+      },
+    },
+  },
+};
 
 const Marquee = () => {
   return (
     <div>
       <div className="marquee">
-        <h1>Hello, World!</h1>
+        <motion.div className="track" variants={marquee} animate="animate">
+          <h1>Hello, World!</h1>
+        </motion.div>
       </div>
     </div>
   );
