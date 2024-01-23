3808074cd77dd025e48c6d47a2b683eb6b634e29
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-23 09:26:02 -0500

    create component for homepage marquee

---
+++ client/components/home/Marquee.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Marquee = () => {
+  return (
+    <div>
+      <div className="marquee">
+        <h1>Hello, World!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Marquee;
