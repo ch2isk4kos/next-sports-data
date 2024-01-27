df40f7f8b985b3e1ef9ec73c66e9e907b84a61b9
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-27 10:36:25 -0500

    import/invoke useState and useEffect hooks to fetch nfl scores on component load

--- client/components/home/Marquee.jsx
+++ client/components/home/Marquee.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { getNFLScores } from "@/lib/api/getNFLScores";
 import { motion } from "framer-motion";
 
 const marqueeVariants = {
@@ -16,16 +17,28 @@ const marqueeVariants = {
 };
 
 const Marquee = () => {
+  const [nflScores, setNFLScores] = useState([]);
+
+  useEffect(() => {
+    getNFLScores().then((res) => setNFLScores(res));
+  }, []);
+
+  console.log("Marquee scores:", nflScores);
+
   return (
     <div className="border-2 border-solid-blue">
       <div className="relative w-full max-w-full h-[206px] overflow-hidden">
-        <motion.div
-          className="whitespace-nowrap"
-          variants={marqueeVariants}
-          animate="animate"
-        >
-          <h1 className="text-9xl">Hello, World!</h1>
-        </motion.div>
+        {nflScores &&
+          nflScores.map((score) => (
+            <motion.div
+              key={score.uid}
+              className="flex flex-row"
+              variants={marqueeVariants}
+              animate="animate"
+            >
+              <h1 className="text-5xl">{score.shortName}</h1>
+            </motion.div>
+          ))}
       </div>
     </div>
   );
