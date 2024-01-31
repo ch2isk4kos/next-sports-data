0c3a788e198bab2a14ca469a0fc7ac46a813cb51
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-30 19:14:20 -0500

    create module to fetch nfl news

---
+++ client/lib/api/getNFLNews.js
@@ -0,0 +1,12 @@
+"use server";
+export async function getNFLScores() {
+  const news = process.env.NFL_NEWS;
+  try {
+    const res = await fetch(`${news}`);
+    const articles = await res.json();
+    console.log("server.events:", articles);
+    return articles;
+  } catch (err) {
+    console.log(err);
+  }
+}
