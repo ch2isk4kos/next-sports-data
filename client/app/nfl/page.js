981bfe6bcf4e8ad7a6296f274594de861e08a47a
ch2isk4kos <ch2isk4kos@gmail.com>
2024-02-01 21:19:34 -0500

    import and render header

--- client/app/nfl/page.js
+++ client/app/nfl/page.js
@@ -1,10 +1,11 @@
-// import Home from "../components/home";
+import Header from "@/components/nfl/Header";
 
 export default function NFLHomepage() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <main className="flex justify-center items-center">
+    <main className="flex flex-col justify-center items-center">
       <h1>NFL Homepage</h1>
+      <Header />
     </main>
   );
 }
